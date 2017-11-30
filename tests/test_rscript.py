ab6c853811bc7730d964f8fd181b01af226abd51
chairco <chairco@gmail.com>
2017-11-30 22:57:56 +0800

    Update test

--- tests/test_rscript.py
+++ tests/test_rscript.py
@@ -4,8 +4,21 @@ import pytest
 
 from pyRscript import pyRscript
 
-
-def test_script():
+@pytest.fixture
+def rscript():
     RPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'R')
-    r = pyRscript.Rscript(path=RPATH, file='test.R', cmd='-t,tlcd0501,-s,2017-07-13 00:00:00,-e,2017-07-14 00:00:00')
-    assert str(type(r)) == "<class 'pyRscript.pyRscript.Rscript'>"  
+    CMD = '-t,tlcd0501,-s,2017-07-13 00:00:00,-e,2017-07-14 00:00:00'
+    r = pyRscript.Rscript(path=RPATH, file='test.R', cmd=CMD)
+    return r
+
+
+def test_repr(rscript):
+    RPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'R')
+    CMD = '-t,tlcd0501,-s,2017-07-13 00:00:00,-e,2017-07-14 00:00:00'
+    assert str(rscript) == '<Parameter test.R {} {}>'.format(RPATH, CMD.split(','))
+
+
+def test_script(rscript):
+    ret = rscript.execute()
+    assert str(type(ret)) == "<class 'pyRscript.pyRscript.ParsedCompletedCommand'>"
+    assert ret.returncode == 0
